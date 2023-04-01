be6280e40cd10b48f6187cc4e0c6df3a4357bbce
nic0michael <nicomichael2012@gmail.com>
2023-04-01 17:53:48 +0200

    Create Cluster Examples

--- K3S-Standard_Cluster/DeleteMasterNode.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# DeleteMasterNode.sh
-#
-/usr/local/bin/k3s-killall.sh
-/usr/local/bin/k3s-uninstall.sh
--- K3S-Standard_Cluster/DeleteWorkerNode.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# DeleteWorkerNode.sh
-#
-/usr/local/bin/k3s-killall.sh
-/usr/local/bin/k3s-agent-uninstall.sh
