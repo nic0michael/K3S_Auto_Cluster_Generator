a4eca8225f4cf05e72aa277f4d27352d5d7401a2
Nehemoth <Nehemoth@users.noreply.github.com>
2024-01-17 14:22:44 -0400

    Update First-Worker-Node-ip-95.sh

--- Examples/K3S-Alpine_Cluster/First-Worker-Node-ip-95.sh
+++ Examples/K3S-Alpine_Cluster/First-Worker-Node-ip-95.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # First-Worker-Node-ip-95.sh
-#######################
+############################
 # First Worker Node #
 #######################
-# IP Address: 10.154.2.95
-# FQDN: leopard.loseyourip.com
-# User ID: nickm
-# Primary Master Node IP Address: 10.154.2.93
+# IP Address: 192.168.1.83
+# FQDN: k3smaster.hellheim.info
+# User ID: nehemoth
+# Primary Master Node IP Address: 192.168.1.81
 # 
 k3s_token="K1086a8d54a028161f9edddb36941a6b559396a23d7be37933657a2aabfbcfc2911::server:484d90102f28bfec5808529ff4673729"
 k3s_url="https://192.168.1.81:6443"
