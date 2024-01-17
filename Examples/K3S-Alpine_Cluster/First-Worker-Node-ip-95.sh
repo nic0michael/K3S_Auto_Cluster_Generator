fefc2e52e9efa5104bfd34a3aec5e5ed0f5610d9
Nehemoth <Nehemoth@users.noreply.github.com>
2024-01-17 14:20:02 -0400

    Update First-Worker-Node-ip-95.sh

--- Examples/K3S-Alpine_Cluster/First-Worker-Node-ip-95.sh
+++ Examples/K3S-Alpine_Cluster/First-Worker-Node-ip-95.sh
@@ -9,6 +9,6 @@
 # Primary Master Node IP Address: 10.154.2.93
 # 
 k3s_token="K1086a8d54a028161f9edddb36941a6b559396a23d7be37933657a2aabfbcfc2911::server:484d90102f28bfec5808529ff4673729"
-k3s_url="https://10.154.2.93:6443"
-node_name="leopard.loseyourip.com"
+k3s_url="https://192.168.1.81:6443"
+node_name="k3smaster.hellheim.info"
 curl -sfL https://get.k3s.io | K3S_URL=${k3s_url} K3S_TOKEN=${k3s_token} K3S_NODE_NAME=${node_name} sh -
