80a37d04141494e1b3edceadbc5c0a769b84d398
Nico Michael <nic0michael@users.noreply.github.com>
2025-03-08 09:40:38 +0200

    Update Worker-Node.sh

--- Small-Cluster/Worker-Node.sh
+++ Small-Cluster/Worker-Node.sh
@@ -3,12 +3,12 @@
 #######################
 # First Worker Node #
 #######################
-# IP Address: 10.154.2.91
+# IP Address: 10.154.2.42
 # FQDN: worker-node
 # User ID: nickm
-# Primary Master Node IP Address: 10.154.2.85
+# Primary Master Node IP Address: 10.154.2.41
 # 
 k3s_token="K105db82e39a8ec3a05de569a7a2e4ffdf2fa3c1db21f87fad5b22e764af61d0d96::server:e54cb4ab0d4026b645c8156477ac7220"
-k3s_url="https://10.154.2.85:6443"
+k3s_url="https://10.154.2.41:6443"
 node_name="worker-node"
 curl -sfL https://get.k3s.io | K3S_URL=${k3s_url} K3S_TOKEN=${k3s_token} K3S_NODE_NAME=${node_name} sh -
